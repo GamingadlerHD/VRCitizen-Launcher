7689ae7728b9b1185bf3ec381cb1ec7e0872f11c
GamingadlerHD <jja146072@stud.gibb.ch>
2025-06-06 18:00:53 +0200

    add functions for vorpx excludes

--- utils/iniFile_utils.py
+++ utils/iniFile_utils.py
@@ -14,14 +14,46 @@ def get_ini_content(ini_path):
     
     return ini_content
 
-def update_ini_property(ini_path, section, key, value):
+def update_or_add_ini_property(ini_path, section, key, value):
     config = configparser.ConfigParser()
     config.read(ini_path, encoding='utf-8')
 
     if not config.has_section(section):
         config.add_section(section)
     
-    config.set(section, key, str(value))
+    if not config.has_option(section, key):
+        config.set(section, key, str(value))
+    else:
+        current_value = config.get(section, key)
+        if current_value != str(value):
+            config.set(section, key, str(value))
 
     with open(ini_path, 'w', encoding='utf-8') as configfile:
         config.write(configfile)
+
+def add_item_to_list_if_needed(exe_name, file_path):
+    config = configparser.ConfigParser()
+    config.read(file_path)
+
+    if 'Excludes' not in config:
+        config['Excludes'] = {}
+
+    excludes = config['Excludes']
+
+    # Prüfen, ob exe schon vorhanden ist
+    if exe_name.lower() in (v.lower() for v in excludes.values()):
+        print(f"{exe_name} ist bereits enthalten.")
+        return
+
+    # Nächsten verfügbaren Schlüssel finden
+    index = 100
+    while f'excl{index}' in excludes:
+        index += 1
+
+    # Hinzufügen
+    excludes[f'excl{index}'] = exe_name
+
+    # Datei speichern
+    with open(file_path, 'w') as configfile:
+        config.write(configfile)
+    print(f"{exe_name} wurde hinzugefügt als excl{index}.")
--- utils/vorpX_utils.py
+++ utils/vorpX_utils.py
@@ -1,14 +1,14 @@
-from utils.iniFile_utils import update_ini_property
+from utils.iniFile_utils import update_or_add_ini_property
 from constants import VORPCONTROL_INI, VORPX_INI
 
 
 
 def update_vrtDis_settings(bEnable, bManualAttach, bNoDisplayAttach, bHeadsetActivityAttach, customResolution=None):
     section = 'VirtualDisplay'
-    update_ini_property(VORPCONTROL_INI, section, 'bEnable', bEnable)
-    update_ini_property(VORPCONTROL_INI, section, 'bManualAttach', bManualAttach)
-    update_ini_property(VORPCONTROL_INI, section, 'bNoDisplayAttach', bNoDisplayAttach)
-    update_ini_property(VORPCONTROL_INI, section, 'bHeadsetActivityAttach', bHeadsetActivityAttach)
+    update_or_add_ini_property(VORPCONTROL_INI, section, 'bEnable', bEnable)
+    update_or_add_ini_property(VORPCONTROL_INI, section, 'bManualAttach', bManualAttach)
+    update_or_add_ini_property(VORPCONTROL_INI, section, 'bNoDisplayAttach', bNoDisplayAttach)
+    update_or_add_ini_property(VORPCONTROL_INI, section, 'bHeadsetActivityAttach', bHeadsetActivityAttach)
 
 def update_headset_settings(headsetType):
     return
